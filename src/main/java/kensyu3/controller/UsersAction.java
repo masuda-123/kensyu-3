e21734ec7ef572236c643111b8b3790d73a4db23
masuda <r_masuda@emyus.jp>
2024-06-20 16:31:20 +0900

    UsersAction.javaを作成

---
+++ src/main/java/kensyu3/controller/UsersAction.java
@@ -0,0 +1,13 @@
+package kensyu3.controller;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+
+public class UsersAction extends ActionSupport{
+	
+	//login画面を表示
+	public String login() throws Exception{
+		return "success";
+	}
+
+}
