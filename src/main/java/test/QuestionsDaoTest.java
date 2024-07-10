83523d2bad93781506c80cf08ce34574eaa5546a
masuda <r_masuda@emyus.jp>
2024-07-10 17:24:44 +0900

    JUnitをインストールし、テストクラスを作成

---
+++ src/main/java/test/QuestionsDaoTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.apache.struts2.StrutsSpringTestCase;
+import org.junit.Test;
+
+import com.opensymphony.xwork2.ActionProxy;
+
+
+class QuestionsDaoTest extends StrutsTestCase {
+
+	@Test
+	void test() {
+		succe("まだ実装されていません");
+	}
+
+}
