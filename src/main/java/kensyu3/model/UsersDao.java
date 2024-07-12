e4410224989fb36cfc204b54abb0d99623d61a59
masuda <r_masuda@emyus.jp>
2024-07-12 14:00:17 +0900

    UsersDaoTestクラスを作成し、テストを追加

--- src/main/java/kensyu3/model/UsersDao.java
+++ src/main/java/kensyu3/model/UsersDao.java
@@ -2,7 +2,6 @@ package kensyu3.model;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
 
 public class UsersDao extends ConnectionDao {
 
@@ -12,50 +11,6 @@ public class UsersDao extends ConnectionDao {
 	public UsersDao() throws Exception {
 		//DBと接続する
 		setConnection();
-	}
-
-	/**
-	 * users テーブルを全件取得
-	 */
-	public ArrayList<UsersBean> findAll() throws Exception {
-		//DBと接続がない場合、接続
-		if (con == null) {
-			setConnection();
-		}
-		//users からidとnameとpasswordを取得（条件：deleted_atが空であること）
-		String sql = "SELECT id, name, password FROM users WHERE deleted_at is null";
-		
-		/** PreparedStatement オブジェクトの取得とsqlの実行**/
-		try(PreparedStatement st = con.prepareStatement(sql); ResultSet rs = st.executeQuery()){
-			/** select文の結果をArrayListに格納 **/
-			ArrayList<UsersBean> list = new ArrayList<UsersBean>();
-			//実行結果を一行ずつ読み込む
-			while (rs.next()) {
-				//実行結果からデータを取得し、各フィールドに格納
-				int id = rs.getInt("id");
-				String name = rs.getString("name");
-				String pass = rs.getString("password");
-				//オブジェクトを作成し、コンストラクタに値を渡す
-				UsersBean bean = new UsersBean(id, name, pass);
-				//listにオブジェクトを追加
-				list.add(bean);
-			}
-			//listを返す（実行結果があればオブジェクトが格納されているが、実行結果がなければ空）
-			return list;
-		} catch (Exception e) {
-			//スタックトレースを出力
-			e.printStackTrace();
-			//例外を投げる
-			throw new DAOException("レコードの取得に失敗しました");
-		} finally {
-			//リソースの開放
-			try {
-				close();
-			} catch (Exception e) {
-				e.printStackTrace();
-				throw new DAOException("リソースの開放に失敗しました");
-			}
-		}
 	}
 	
 	/**
